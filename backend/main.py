9dcc05b2d6aa35fd683c71ad2ebed4b34b6efff5
Blehhz <arvind.gamer.death@gmail.com>
2026-02-16 10:24:01 +0530

    frontend - backend link bug fix

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from api.health import router as health_router
 from api.analyze import router as analyze_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Neuro Diagnosis Backend",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(health_router)
